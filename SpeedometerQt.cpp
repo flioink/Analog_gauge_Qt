88d0cc28770a65cc5e2775c8bd8f9809e93b2c4b
flioink <phantom_warrior@abv.bg>
2025-10-29 13:18:22 +0200

    disk gauge and borderless window added

--- SpeedometerQt.cpp
+++ SpeedometerQt.cpp
@@ -8,28 +8,53 @@
 #include <QPropertyAnimation>
 #include <QSequentialAnimationGroup>
 #include <QTimer>
+#include <QMouseEvent>
+#include <QPoint>
 
-#include<windows.h>
 
-int WIDTH = 220;
+int WIDTH = 160;
 int HEIGHT = WIDTH;
 
-double test_needle_start = -180;
-double test_rotation_range = 2.6; // max angle  
+
 
 RadialGauge::RadialGauge(QWidget* parent)
     : QMainWindow(parent)
 {           
     build_UI();
-    setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
+   
+    setWindowFlags(Qt::FramelessWindowHint);
 
     adjustSize();
     setFixedSize(size());
-    connect_signals();   
+    connect_button_signals();  
+
+    setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
 }
 
 RadialGauge::~RadialGauge()
 {}
+
+//mouse events overrides
+void RadialGauge::mousePressEvent(QMouseEvent* event) 
+{
+    if (event->button() == Qt::LeftButton) // if left button pressed
+    {
+        // get current mouse position and subtract the topleft corner position from it
+        m_drag_position = event->globalPosition().toPoint() - frameGeometry().topLeft();
+        event->accept();
+    }
+    
+}
+
+void RadialGauge::mouseMoveEvent(QMouseEvent* event) 
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        // on drag - offset position from the mouse position to avoid jumps 
+        move(event->globalPosition().toPoint() - m_drag_position);
+        event->accept();
+    }
+}
 
 void RadialGauge::build_UI()
 {
@@ -38,80 +63,30 @@ void RadialGauge::build_UI()
     QWidget* central_widget = new QWidget(this);
     QVBoxLayout* master_layout = new QVBoxLayout(central_widget);
 
-    QHBoxLayout* gauges_area = new QHBoxLayout();
-    QHBoxLayout* buttons_area = new QHBoxLayout();
+    m_gauges_area = new QHBoxLayout();
+    m_buttons_area = new QHBoxLayout();
 
-    m_main_slider1 = new QSlider(Qt::Horizontal, this);
-   
-    
-    m_main_slider1->setRange(0, 100);
-    
+    /*m_main_slider1 = new QSlider(Qt::Horizontal, this);    
+    m_main_slider1->setRange(0, 100);*/    
    
     m_main_button = new QPushButton("Gauge Test", this);
-
     m_system_monitor = new SystemMonitor(this);
-    QTimer* timer = new QTimer(this);
+    m_timer = new QTimer(this);
 
+    create_cpu_gauge();
 
-    // custom gauge
-    m_cpu_gauge = new AnalogGauge(test_needle_start, test_rotation_range, "./gauge0.png", this);
-    m_cpu_gauge->setMinimumSize(WIDTH, WIDTH); 
-    // add gauge object to layout
-    gauges_area->addWidget(m_cpu_gauge);
+    create_memory_gauge();
 
+    create_disk_gauge();  
+    
 
-    connect(timer, &QTimer::timeout, this, [this]() 
-     { 
-        double cpu = m_system_monitor->get_cpu_usage();
-        qDebug() << "CURRENT CPU LOAD: " << cpu;
+    m_timer->start(100); // Update every n milliseconds       
         
-        if (cpu < 1)
-        { 
-            cpu = m_last_good_value; // keep previous value if current falls to zero to prevent jitter
-        } 
-        else m_last_good_value = cpu;
-
-        
-
-        m_cpu_gauge->set_speed(cpu);  
-
-        qDebug() << "MEMORY % USED: " << m_system_monitor->get_memory_usage();
-            
-     });
-
-
-    // custom gauge
-    m_memory_gauge = new AnalogGauge(-155, 3.0, "./gauge3.jpg", this);
-    m_memory_gauge->setMinimumSize(WIDTH, WIDTH);
-    // add gauge object to layout
-    gauges_area->addWidget(m_memory_gauge);
-
-    connect(timer, &QTimer::timeout, this, [this]()
-        {
-
-            auto mem_perc_used = m_system_monitor->get_memory_usage();
-
-            qDebug() << "MEMORY % USED: " << m_system_monitor->get_memory_usage();
-
-
-
-            m_memory_gauge->set_speed(mem_perc_used);
-
-            
-
-        });
-
     
+    m_buttons_area->addWidget(m_main_button);
 
-    timer->start(60); // Update every n milliseconds   
-    
-    
-    buttons_area->addWidget(m_main_slider1);    
-    
-    buttons_area->addWidget(m_main_button);
-
-    master_layout->addLayout(gauges_area, 5);
-    master_layout->addLayout(buttons_area, 1);
+    master_layout->addLayout(m_gauges_area, 5);
+    master_layout->addLayout(m_buttons_area, 1);
 
 
     master_layout->setAlignment(Qt::AlignTop);
@@ -119,23 +94,115 @@ void RadialGauge::build_UI()
 
 }
 
-void RadialGauge::connect_signals()
+
+void RadialGauge::create_cpu_gauge()
+{
+    // CPU gauge
+    m_cpu_gauge = new AnalogGauge(-180, 2.6, "./gauge0.png", this);
+    m_cpu_gauge->setMinimumSize(WIDTH, WIDTH);
+    // add gauge object to layout
+    m_gauges_area->addWidget(m_cpu_gauge);
+
+
+    connect(m_timer, &QTimer::timeout, this, [this]()
+        {
+            double cpu = m_system_monitor->get_cpu_usage();
+            qDebug() << "CURRENT CPU LOAD: " << cpu;
+
+            if (cpu < 1)
+            {
+                cpu = m_last_good_cpu_value; // keep previous value if current falls to zero to prevent jitter
+            }
+            else m_last_good_cpu_value = cpu;
+
+
+            if (!m_pause)
+            {
+                m_cpu_gauge->set_speed(cpu);
+            }
+
+        });
+
+}
+
+
+void RadialGauge::create_memory_gauge()
+{
+
+    // memory gauge
+    m_memory_gauge = new AnalogGauge(-155, 3.0, "./gauge3.jpg", this);
+    m_memory_gauge->setMinimumSize(WIDTH, WIDTH);
+    // add gauge object to layout
+    m_gauges_area->addWidget(m_memory_gauge);
+
+    connect(m_timer, &QTimer::timeout, this, [this]()
+        {
+            auto mem_perc_used = m_system_monitor->get_memory_usage();
+
+            qDebug() << "MEMORY % USED: " << m_system_monitor->get_memory_usage();
+
+            if (!m_pause)
+            {
+                m_memory_gauge->set_speed(mem_perc_used);
+            }
+        });
+
+}
+
+
+void RadialGauge::create_disk_gauge()
+{
+    m_disk_gauge = new AnalogGauge(-135, 2.6, "./gauge4.jpg", this);
+    m_disk_gauge->setMinimumSize(WIDTH, WIDTH);
+    // add gauge object to layout
+    m_gauges_area->addWidget(m_disk_gauge);
+
+    double end_point = m_disk_gauge->get_gauge_end_position(); // this is the gauge end point based on m_end_position = 100 * m_rotation_range + m_remap_value
+
+    connect(m_timer, &QTimer::timeout, this, [this, end_point]()
+        {
+
+            auto disk_speed = m_system_monitor->get_disk_read_speed();
+
+            double mb_per_sec = disk_speed / (1024 * 1024);
+
+            if (mb_per_sec < 0.1) mb_per_sec = 0.0;
+
+            // limit the needle so it doesn't go crazy at very high values
+            double clamped = std::min(mb_per_sec, end_point);
+
+            qDebug() << "DISK SPEED in MB: " << clamped;
+
+            if (!m_pause)
+            {
+                m_disk_gauge->set_speed(mb_per_sec);
+            }
+        });
+
+}
+
+
+void RadialGauge::connect_button_signals()
 { 
    // connect the slider from the "stage" class to the "gauge" class method that sets speed 
    connect(m_main_slider1, &QSlider::valueChanged, this, [this](int value){ m_cpu_gauge->set_speed(value); });
-   /*connect(m_main_slider2, &QSlider::valueChanged, this, [this](int value){ m_speed_gauge_2->set_speed(value); });
-   connect(m_main_slider3, &QSlider::valueChanged, this, [this](int value){ m_speed_gauge_3->set_speed(value); });*/
+   
 
    connect(m_main_button, &QPushButton::clicked, this, [this](int value)
        {
            m_main_button->setEnabled(false);
+           m_pause = true;
            m_cpu_gauge->move_needle(); 
+           m_memory_gauge->move_needle();
+           m_disk_gauge->move_needle();
 
-           QTimer::singleShot( 2000, this, [this]() { m_main_button->setEnabled(true);} );
-       });
-
-
-   
+           QTimer::singleShot( 2000, this, [this]() 
+               { 
+                m_main_button->setEnabled(true);
+                m_pause = false; 
+               } 
+           );
+       });  
 
 }
 
@@ -152,9 +219,11 @@ int cover_cap_radius = WIDTH * 0.04;
 AnalogGauge::AnalogGauge(double needle_start_pos, double max_range, QString bg, QWidget* parent):
     m_current_angle(needle_start_pos), m_rotation_range(max_range), m_remap_value(needle_start_pos)
 {   
+    m_end_position = 100 * m_rotation_range + m_remap_value;
+
     load_bg(bg);
     
-    load_assets();
+    set_needle_pivot();
 }
 
 void AnalogGauge::paintEvent(QPaintEvent* event)
@@ -167,7 +236,7 @@ void AnalogGauge::paintEvent(QPaintEvent* event)
 
     // transforms for needle
    
-    painter.translate(m_center_x, m_center_y);  // move origin to gauge center
+    painter.translate(m_gauge_center_x, m_gauge_center_y);  // move origin to gauge center
     painter.rotate(m_current_angle);         // rotate coordinate system
 
     // needle is drawn in transformed coordinates
@@ -182,15 +251,15 @@ void AnalogGauge::paintEvent(QPaintEvent* event)
     
 }
 
-void AnalogGauge::load_assets()
+void AnalogGauge::set_needle_pivot()
 {
     
     m_needle.load("./speed_arrow.png");
     //m_needle = m_needle.scaledToHeight(100, Qt::SmoothTransformation);
 
-    // needle pivot
-    m_center_x = WIDTH / 2;
-    m_center_y = HEIGHT / 2;
+    // needle pivot is set at the center of the circle
+    m_gauge_center_x = WIDTH / 2;
+    m_gauge_center_y = HEIGHT / 2;
 
     m_needle_width = m_needle.width();
     m_needle_height = m_needle.height();
@@ -224,27 +293,27 @@ void AnalogGauge::move_needle()
     QPropertyAnimation* sweep = new QPropertyAnimation(this, "current_angle");
     int duration = 1000;
 
-    double end_position = 100 * m_rotation_range + m_remap_value;
+    
 
     int pause_duration = 100;
     
     sweep->setDuration(duration); // animation length in miliseconds
     sweep->setStartValue(m_current_angle); // starting angle sets the direction
 
-    if (end_position > 0) // prevents weird behavior if the arrow is set to rotate counter clock-wise
+    if (m_end_position > 0) // prevents weird behavior if the arrow is set to rotate counter clock-wise
     {
         sweep->setKeyValueAt(0.7, 100);  // overshoot
     }
     
 
-    sweep->setKeyValueAt(1, end_position);     // settle
+    sweep->setKeyValueAt(1, m_end_position);     // settle
     sweep->setEasingCurve(QEasingCurve::OutCubic);  // Smooth deceleration into settle
 
     // Return to start  
     QPropertyAnimation* retreat = new QPropertyAnimation(this, "current_angle");
     retreat->setEasingCurve(QEasingCurve::InOutQuad);
     retreat->setDuration(duration);
-    retreat->setStartValue(end_position);
+    retreat->setStartValue(m_end_position);
     retreat->setEndValue(m_current_angle);
 
     group->addAnimation(sweep);
@@ -275,15 +344,30 @@ void AnalogGauge::load_bg(const QString& bg)
 }
 
 
-// system monitor class uses the Pdh header
 
-SystemMonitor::SystemMonitor(QObject* parent): QObject(parent)
+
+// system monitor class uses the Pdh header
+// Declare a query - PDH_HQUERY my_query;
+// Open the query - PdhCollectQueryData(my_query);
+// Declare a counter for my_query - PDH_HCOUNTER my_counter;
+// Set up the counter - PdhAddCounter(my_query, L"\\Processor(_Total)\\% Processor Time", 0, &my_counter);
+// Get the recorded value in a usable format - PdhGetFormattedCounterValue(my_query, PDH_FMT_DOUBLE, NULL, &value);
+
+SystemMonitor::SystemMonitor(QObject* parent): QObject(parent), m_smooth_cpu(0.0), m_smooth_disk(0.0)
 {
+    // Pdh queries
     PdhOpenQuery(NULL, 0, &m_cpu_query);
     PdhAddCounter(m_cpu_query, L"\\Processor(_Total)\\% Processor Time", 0, &m_cpu_counter);
 
     PdhOpenQuery(NULL, 0, &m_memory_query);
     PdhAddCounter(m_memory_query, L"\\Memory\\Available MBytes", 0, &m_memory_counter);
+
+    PdhOpenQuery(NULL, 0, &m_disk_query);
+    PdhAddCounter(m_disk_query, L"\\PhysicalDisk(_Total)\\Disk Read Bytes/sec", 0, &m_disk_counter);
+
+    // these don't show anything
+    //PdhAddCounter(m_disk_query, L"\\LogicalDisk(_Total)\\Disk Read Bytes / sec", 0, &m_disk_counter);
+    //PdhAddCounter(m_disk_query, L"\\LogicalDisk(C:)\\Disk Read Bytes / sec", 0, &m_disk_counter);
 }
 
 SystemMonitor::~SystemMonitor()
@@ -297,11 +381,8 @@ double SystemMonitor::get_cpu_usage()
     PdhGetFormattedCounterValue(m_cpu_counter, PDH_FMT_DOUBLE, NULL, &value);
 
     double current_cpu = value.doubleValue;
-
-    //qDebug() << "CURRENT CPU LOAD: " << current_cpu;
-
-    // low pass
-    m_smooth_cpu = 0.9 * m_smooth_cpu + current_cpu * 0.1;
+    //qDebug() << "CURRENT CPU LOAD: " << current_cpu;    
+    m_smooth_cpu = 0.9 * m_smooth_cpu + current_cpu * 0.1; // low pass
         
     return m_smooth_cpu;
 }
@@ -327,10 +408,20 @@ double SystemMonitor::get_memory_usage()
     return used_memory_percentage;
 }
 
+double SystemMonitor::get_disk_read_speed()
+{
+    PdhCollectQueryData(m_disk_query);
+    PDH_FMT_COUNTERVALUE value;
+    PdhGetFormattedCounterValue(m_disk_counter, PDH_FMT_DOUBLE, NULL, &value);
 
-// 1. "Start recording performance data"
-//PdhCollectQueryData(query); 
+    double current_disk_usage = value.doubleValue;    
 
-// 2. "Get the recorded value in a usable format"  
-//PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, NULL, &value);
-// "Give me that snapshot as a double"
+    m_smooth_disk = 0.9 * m_smooth_disk + current_disk_usage * 0.1;
+
+    //qDebug() << "Cirrent Disk in Kb/s: " << current_disk_usage;
+
+    return m_smooth_disk;
+
+}
+
+
--- SpeedometerQt.h
+++ SpeedometerQt.h
@@ -8,7 +8,11 @@ class QSlider;
 class QPushButton;
 class QPaintEvent;
 class AnalogGauge;
+class QHBoxLayout;
+class QVBoxLayout;
+class QTimer;
 class SystemMonitor;
+class QPoint;
 
 
 class RadialGauge : public QMainWindow
@@ -19,30 +23,40 @@ class RadialGauge : public QMainWindow
     double m_main_slider_value = 0.0;
 
 public slots:
-    void connect_signals();
-    
+    void connect_button_signals();
+    void create_cpu_gauge();
+
+    void create_memory_gauge();
+
+    void create_disk_gauge();
     
 public:
     RadialGauge(QWidget* parent = nullptr);
     ~RadialGauge();    
-    void build_UI();
+    void build_UI();  
 
-    
+protected:
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
 
 private:
-
-    QSlider* m_main_slider1;
-    QSlider* m_main_slider2;
-    QSlider* m_main_slider3;
-
     SystemMonitor* m_system_monitor;
+    QHBoxLayout* m_gauges_area;
+    QHBoxLayout* m_buttons_area;
+    QTimer* m_timer;
+    QPoint m_drag_position;
+
+    QSlider* m_main_slider1;    
 
     QPushButton* m_main_button;
     AnalogGauge* m_cpu_gauge;  
-    AnalogGauge* m_memory_gauge;  
+    AnalogGauge* m_memory_gauge; 
+    AnalogGauge* m_disk_gauge; 
     
 
-    double m_last_good_value = 1.0;
+    double m_last_good_cpu_value = 1.0;
+
+    bool m_pause = false;
 };
 
 
@@ -59,8 +73,8 @@ private:
     QImage m_background;
     QImage m_needle;
 
-    int m_center_x;
-    int m_center_y;
+    int m_gauge_center_x;
+    int m_gauge_center_y;
 
     int m_needle_width;
     int m_needle_height;   
@@ -68,6 +82,8 @@ private:
     double m_rotation_range = 2.63; // max angle 
     double m_current_angle;
     double m_remap_value;
+
+    double m_end_position;      
     
 
 public:
@@ -75,13 +91,13 @@ public:
 
     void paintEvent(QPaintEvent* event) override;
 
-    void load_assets();
+    void set_needle_pivot();
 
     void set_speed(double speed);
 
     double map_speed_to_angle(int speed);
 
-    
+    const double get_gauge_end_position() { return m_end_position; }
 
     void move_needle();   
 
@@ -100,8 +116,9 @@ signals:
     void animation_finished();
 
 };
-    
+  
 
+// system monitor
 class SystemMonitor : public QObject
 {
     Q_OBJECT
@@ -112,14 +129,21 @@ public:
 
     double get_cpu_usage();
     double get_memory_usage();
-    double m_smooth_cpu;
+    double get_disk_read_speed();
+    
 
 private:
+
+    double m_smooth_cpu;
+    double m_smooth_disk;
 
     PDH_HQUERY m_cpu_query;
     PDH_HCOUNTER m_cpu_counter;
 
     PDH_HQUERY m_memory_query;
     PDH_HCOUNTER m_memory_counter;
+
+    PDH_HQUERY m_disk_query;
+    PDH_HCOUNTER m_disk_counter;
 
 };
